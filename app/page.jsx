93cf9e450b21c173ef13688a775b3512852e683a
R3IC0P <luxorpol@gmail.com>
2024-02-19 13:42:53 +0100

    change background style

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ import { ThemeSwitcher } from './components/ThemeSwitcher'
 export default function Home() {
   return (
     <div className="h-full bg-cover bg-center bg-[url('https://images.unsplash.com/photo-1477468572316-36979010099d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGZvZyUyMG1vdW50YWlufGVufDB8fDB8fHww')] dark:bg-[url('https://wallpapersmug.com/download/1024x768/95afc8/dark-night-river-forest-minimal-art.jpg')]">
-      <div className="grid grid-cols-1 grid-rows-[auto 1fr auto] gap-8 items-center h-full p-6 backdrop-blur-md dark:backdrop-brightness-50 lg:w-1/2 lg:bg-slate-100 lg:bg-opacity-70 dark:lg:bg-opacity-70 dark:lg:bg-zinc-950">
+      <div className="grid grid-cols-1 grid-rows-[auto 1fr auto] gap-8 items-center h-full p-6 backdrop-blur-lg lg:w-1/2 lg:bg-opacity-80 dark:lg:bg-opacity-90 lg:bg-slate-100 dark:lg:bg-zinc-950">
         <div className="flex justify-between items-start h-full">
           <div className="sm:pl-10">
             <h1 className="text-xl font-semibold">Company Logo</h1>
