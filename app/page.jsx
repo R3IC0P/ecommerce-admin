4fb3216112cf22dad5c38b3337420d2c6d1ae0c0
R3IC0P <luxorpol@gmail.com>
2024-02-20 20:00:44 +0100

    add testing buttons

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,15 @@
+import { Button } from '@nextui-org/react'
+import Link from 'next/link'
+
 export default function Home() {
-  return <></>
+  return (
+    <>
+      <Button as={Link} href={'/login'} variant="bordered">
+        Zaloguj
+      </Button>
+      <Button as={Link} href={'/register'} variant="bordered">
+        Zarejestruj
+      </Button>
+    </>
+  )
 }
