1ce5c63f09e8d112fb23e5048eff461be5c59420
R3IC0P <luxorpol@gmail.com>
2024-02-20 20:00:19 +0100

    add session provider

--- app/providers.jsx
+++ app/providers.jsx
@@ -2,12 +2,13 @@
 
 import { NextUIProvider } from '@nextui-org/react'
 import { ThemeProvider as NextThemeProvider } from 'next-themes'
+import { SessionProvider } from 'next-auth/react'
 
 export function Providers({ children }) {
   return (
     <NextUIProvider className="h-full">
       <NextThemeProvider attribute="class" defaultTheme="dark">
-        {children}
+        <SessionProvider>{children}</SessionProvider>
       </NextThemeProvider>
     </NextUIProvider>
   )
