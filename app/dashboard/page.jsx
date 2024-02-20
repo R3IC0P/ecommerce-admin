9c2c59f7f859acc64fa19f4a0118cc068c2355d1
R3IC0P <luxorpol@gmail.com>
2024-02-20 19:59:27 +0100

    nothing important ;)

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,15 @@
+'use client'
+
+import { Button } from '@nextui-org/react'
+import { signOut, useSession } from 'next-auth/react'
+import { redirect } from 'next/navigation'
+
 export default function Dashboard() {
-  return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
-  )
+  const { data: session } = useSession()
+
+  if (session) {
+    return <>Zalogowano</>
+  }
+
+  redirect('/')
 }
