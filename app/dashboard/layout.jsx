651d0a7d6a06949d5339dc00c6350b01f7863ecb
R3IC0P <luxorpol@gmail.com>
2024-02-20 11:22:20 +0100

    add topbar & change padding

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,10 +1,12 @@
 import SideMenu from './SideMenu'
+import Topbar from './Topbar'
 
 export default function DashboardLayout({ children }) {
   return (
     <div className="flex h-full">
       <SideMenu />
-      <div className="p-10">
+      <div className="px-10 py-7">
+        <Topbar />
         <main>{children}</main>
       </div>
     </div>
