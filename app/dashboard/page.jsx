f3dc1e125a2fb83eb541b1145bdc5b6abc6f5f9e
R3IC0P <luxorpol@gmail.com>
2024-02-21 11:58:35 +0100

    delete unsused

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,15 +1,3 @@
-'use client'
-
-import { Button } from '@nextui-org/react'
-import { signOut, useSession } from 'next-auth/react'
-import { redirect } from 'next/navigation'
-
 export default function Dashboard() {
-  const { data: session } = useSession()
-
-  if (session) {
-    return <>Zalogowano</>
-  }
-
-  redirect('/')
+  return <>Zalogowano</>
 }
