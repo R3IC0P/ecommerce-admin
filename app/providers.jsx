0c7a0541e6617ebf4b1e3133bf4c2df27c758963
R3IC0P <luxorpol@gmail.com>
2024-02-22 18:16:31 +0100

    delete session provider

--- app/providers.jsx
+++ app/providers.jsx
@@ -2,13 +2,12 @@
 
 import { NextUIProvider } from '@nextui-org/react'
 import { ThemeProvider as NextThemeProvider } from 'next-themes'
-import { SessionProvider } from 'next-auth/react'
 
 export function Providers({ children }) {
   return (
     <NextUIProvider className="h-full">
       <NextThemeProvider attribute="class" defaultTheme="dark">
-        <SessionProvider>{children}</SessionProvider>
+        {children}
       </NextThemeProvider>
     </NextUIProvider>
   )
