5a2ba0dea4a2733c90220f84dbdb08476f830150
R3IC0P <luxorpol@gmail.com>
2024-02-22 19:41:41 +0100

    component rename & flex to grid

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,6 +1,6 @@
 import { auth } from '@/auth'
-import SideMenu from './SideMenu'
 import Topbar from './Topbar'
+import SideBar from './SideBar'
 
 export default async function DashboardLayout({ children }) {
   const session = await auth()
@@ -11,8 +11,8 @@ export default async function DashboardLayout({ children }) {
   }
 
   return (
-    <div className="flex h-full">
-      <SideMenu userData={userData} />
+    <div className="grid grid-cols-[auto_1fr] h-full">
+      <SideBar userData={userData} />
       <div className="px-10 py-7">
         <Topbar />
         <main>{children}</main>
