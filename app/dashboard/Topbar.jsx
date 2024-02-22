6e13936af67ef798b09ad0f4b79b4ed83b88f0cf
R3IC0P <luxorpol@gmail.com>
2024-02-22 19:24:15 +0100

    add breadcrumbs based on location

--- app/dashboard/Topbar.jsx
+++ app/dashboard/Topbar.jsx
@@ -1,16 +1,45 @@
 'use client'
 
-import { BreadcrumbItem, Breadcrumbs } from '@nextui-org/react'
+import { BreadcrumbItem, Breadcrumbs, SelectItem } from '@nextui-org/react'
+import { usePathname } from 'next/navigation'
 import { MdHome } from 'react-icons/md'
 
 export default function Topbar() {
+  const matchCrumb = (crumb) => {
+    const crumbNames = {
+      dashboard: 'Panel',
+      help: 'Pomoc',
+      messages: 'Wiadomości',
+      orders: 'Zamówienia',
+      settings: 'Ustawienia',
+      tasks: 'Zadania',
+      users: 'Użytkownicy',
+      warehouse: 'Magazyn',
+    }
+    return crumbNames[crumb]
+  }
+
+  let currentLink = ''
+  const crumbs = usePathname()
+    .split('/')
+    .filter((crumb) => crumb !== '')
+    .map((crumb) => {
+      currentLink += `/${crumb}`
+      const crumbName = matchCrumb(crumb)
+      return (
+        <BreadcrumbItem key={crumb} href={currentLink}>
+          {crumbName}
+        </BreadcrumbItem>
+      )
+    })
+
   return (
     <div>
       <Breadcrumbs>
-        <BreadcrumbItem>
+        <BreadcrumbItem key="home" href="/">
           <MdHome />
         </BreadcrumbItem>
-        <BreadcrumbItem>Panel</BreadcrumbItem>
+        {crumbs}
       </Breadcrumbs>
     </div>
   )
