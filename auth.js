35f2f7dfa8b9e4a1831ff33d675e4c1bc7e15241
R3IC0P <luxorpol@gmail.com>
2024-02-22 18:18:59 +0100

    fully rebuild

--- auth.js
+++ auth.js
@@ -1,39 +1,68 @@
 import NextAuth from 'next-auth'
+
+import GitHub from 'next-auth/providers/github'
 import Google from 'next-auth/providers/google'
 import Credentials from 'next-auth/providers/credentials'
 
-export const {
-  handlers: { GET, POST },
-  auth,
-} = NextAuth({
+import { connectMongoDB } from './lib/mongoose'
+import User from './models/user'
+import bcrypt from 'bcryptjs'
+
+export const handler = NextAuth({
   providers: [
-    Google({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
     Credentials({
+      name: 'Credentials',
       credentials: {
-        email: {},
-        password: {},
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
       },
-      async authorize(credentials, req) {
-        // const res = await fetch('/your/endpoint', {
-        //   method: 'POST',
-        //   headers: { 'Content-Type': 'application/json' },
-        // })
-        // const user = await res.json()
-        // if (res.ok && user) {
-        //   return user
-        // }
-        // return null
+      async authorize(credentials) {
+        await connectMongoDB()
+
+        const user = await User.findOne({ email: credentials.email })
+        const isPasswordMatch = await bcrypt.compare(
+          credentials.password,
+          user.password
+        )
+
+        const { name, email, rule, position, image } = user
+
+        if (user && isPasswordMatch) {
+          return { name, email, rule, position, image }
+        }
+        return null
       },
     }),
+    GitHub,
+    Google,
   ],
-  session: {
-    strategy: 'jwt',
+  callbacks: {
+    authorized({ request, auth }) {
+      const { pathname } = request.nextUrl
+      if (pathname.startsWith('/dashboard')) return !!auth
+      return true
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        // token.accessToken = user.access_token
+        return { ...token, ...user }
+      }
+      return token
+    },
+    async session({ session, token }) {
+      // session.accessToken = token.accessToken
+      session.user.id = token.id
+      session.user.rule = token.rule
+      session.user.position = token.position
+      return session
+    },
   },
-  secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: '/login',
   },
+  session: {
+    strategy: 'jwt',
+  },
 })
+
+export const { handlers, auth, signIn, signOut } = handler
