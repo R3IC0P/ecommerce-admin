06c0ca957cf01278e715968c519fabeeb1737518
R3IC0P <luxorpol@gmail.com>
2024-02-22 18:13:52 +0100

    add session data

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,10 +1,18 @@
+import { auth } from '@/auth'
 import SideMenu from './SideMenu'
 import Topbar from './Topbar'
 
-export default function DashboardLayout({ children }) {
+export default async function DashboardLayout({ children }) {
+  const session = await auth()
+  const userData = {
+    name: session.user.name,
+    position: session.user.position,
+    image: session.user.image,
+  }
+
   return (
     <div className="flex h-full">
-      <SideMenu />
+      <SideMenu userData={userData} />
       <div className="px-10 py-7">
         <Topbar />
         <main>{children}</main>
