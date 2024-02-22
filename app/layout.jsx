e2c844be7d6e3ae76696a66fe6d040007cc9a803
R3IC0P <luxorpol@gmail.com>
2024-02-22 18:17:22 +0100

    add session provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,17 @@
 import { Providers } from './providers'
 import './globals.css'
+import { SessionProvider } from 'next-auth/react'
+import { auth } from '@/auth'
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+  const session = await auth()
+
   return (
     <html lang="pl" suppressHydrationWarning>
       <body className="h-screen">
-        <Providers>{children}</Providers>
+        <SessionProvider session={session}>
+          <Providers>{children}</Providers>
+        </SessionProvider>
       </body>
     </html>
   )
