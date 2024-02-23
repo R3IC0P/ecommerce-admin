87a3a40672287b9f7f57b9dc101163fa2d23b74c
R3IC0P <luxorpol@gmail.com>
2024-02-23 09:44:27 +0100

    add bar at the top for smaller screen

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,6 +1,6 @@
 import { auth } from '@/auth'
 import Topbar from './Topbar'
-import SideBar from './SideBar'
+import SideBar from './Sidebar'
 
 export default async function DashboardLayout({ children }) {
   const session = await auth()
@@ -11,11 +11,14 @@ export default async function DashboardLayout({ children }) {
   }
 
   return (
-    <div className="grid grid-cols-[auto_1fr] h-full">
+    <div className="grid lg:grid-cols-[auto_1fr] h-full">
       <SideBar userData={userData} />
-      <div className="px-10 py-7">
-        <Topbar />
-        <main>{children}</main>
+      <div>
+        <div className="h-10 border border-neutral-300 bg-neutral-100 dark:border-neutral-800 dark:bg-neutral-900 lg:hidden"></div>
+        <div className="px-10 py-7">
+          <Topbar />
+          <main>{children}</main>
+        </div>
       </div>
     </div>
   )
